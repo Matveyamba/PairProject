402eb23f2e657a5b671e1cd12d32c216fb99d4bf
Your name <matya2303@mail.ru>
2023-11-18 23:31:34 +0300

    main.cpp file completed

--- Project/main.cpp
+++ Project/main.cpp
@@ -1,10 +1,36 @@
+// main.cpp
 #include <iostream>
+#include "func.h"
 
 using namespace std;
 
-int main()
-{
-    cout << "Hello World!" << endl;
+int main() {
+    double a, b;
+    char operation;
+
+    cout << "Enter two numbers: ";
+    cin >> a >> b;
+
+    cout << "Choose operation (+, -, *, /): ";
+    cin >> operation;
+
+    switch (operation) {
+        case '+':
+            cout << "Result: " << Func::add(a, b) << endl;
+            break;
+        case '-':
+            cout << "Result: " << Func::subtract(a, b) << endl;
+            break;
+        case '*':
+            cout << "Result: " << Func::multiply(a, b) << endl;
+            break;
+        case '/':
+            cout << "Result: " << Func::divide(a, b) << endl;
+            break;
+        default:
+            cout << "Incorrect operation!" << endl;
+            break;
+    }
+
     return 0;
 }
-
